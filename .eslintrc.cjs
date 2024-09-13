ecbeb8037005d83d2ae45d913d57e60929276cb9
Bruno Skëndaj <skendajbruno07@gmail.com>
2024-09-13 03:06:47 +0200

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,6 @@ module.exports = {
       { allowConstantExport: true },
      
     ],
-    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true }]
   },
 }
